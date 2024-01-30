91c5db9dee71467e27f6b3e75f9002f6edd446db
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-01-30 16:43:50 +0200

    CSS updates

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -52,7 +52,7 @@ function Home() {
                     <div className={styles["card-body"]}>
                         <h4>Vostok-Europe SSN-571</h4>
                         <div className={styles["price-box"]}>
-                            <span className={styles["reg-price"]}>1508.00 лв.</span>
+                            <span className={styles["reg-price"]}>1508 лв.</span>
                         </div>
                     </div>
                 </div>
@@ -63,8 +63,8 @@ function Home() {
                     <div className={styles["card-body"]}>
                         <h4>Vostok-Europe Atomic Age Automatic Watch</h4>
                         <div className={styles["price-box"]}>
-                            <span className={styles["old-price"]}>1905.00 лв.</span>
-                            <span className={styles["new-price"]}>1850.00 лв.</span>
+                            <span className={styles["old-price"]}>1905 лв.</span>
+                            <span className={styles["new-price"]}>1850 лв.</span>
                         </div>
                     </div>
                 </div>
@@ -75,7 +75,7 @@ function Home() {
                     <div className={styles["card-body"]}>
                         <h4>Vostok-Europe SSN-571</h4>
                         <div className={styles["price-box"]}>
-                            <span className={styles["reg-price"]}>1508.00 лв.</span>
+                            <span className={styles["reg-price"]}>1508 лв.</span>
                         </div>
                     </div>
                 </div>
@@ -86,7 +86,7 @@ function Home() {
                     <div className={styles["card-body"]}>
                         <h4>ORIENT STAR CLASSIC OPEN HEART 41MM</h4>
                         <div className={styles["price-box"]}>
-                            <span className={styles["reg-price"]}>1083.00 лв.</span>
+                            <span className={styles["reg-price"]}>1083. лв.</span>
                         </div>
                     </div>
                 </div>
@@ -109,8 +109,8 @@ function Home() {
                     <div className={styles["card-body"]}>
                         <h4>Seiko Prospex SNJ029P 'Arnie' Solar Divers Watch</h4>
                         <div className={styles["price-box"]}>
-                            <span className={styles["old-price"]}>1199.00 лв.</span>
-                            <span className={styles["new-price"]}>1099.00 лв.</span>
+                            <span className={styles["old-price"]}>1199 лв.</span>
+                            <span className={styles["new-price"]}>1099 лв.</span>
                         </div>
                     </div>
                 </div>
