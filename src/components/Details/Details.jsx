54454528e45f42a55e711fec2344b9bbc54e8d86
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-02-01 14:44:50 +0200

    Add row similar watches

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -82,6 +82,54 @@ function Details() {
                     <p>Водоустойчива и удароустойчива кутия за съхранение и транспортиране на часовника и аксесоарите.</p>
                 </div>
             </div>
+            <h3 className={styles["row-similar-title"]}>Разгледайте също:</h3>
+            <div className={styles["row-similar-watches"]}>
+                <div className={styles["watch-card"]}>
+                    <div className={styles["img-box"]}>
+                        <img src="https://cdncloudcart.com/16251/products/images/3108/vostok-uss-ssn-571-46mm-energia-automatic-mazki-casovnik-vk61-571c612-image_610e90589946d_800x800.png?1628345727" alt="" />
+                    </div>
+                    <div className={styles["card-body"]}>
+                        <h4>Vostok-Europe SSN-571</h4>
+                        <div className={styles["price-box"]}>
+                            <span className={styles["reg-price"]}>1508 лв.</span>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles["watch-card"]}>
+                    <div className={styles["img-box"]}>
+                        <img src="https://cdncloudcart.com/16251/products/images/6541/vostok-europe-48mm-atomic-age-mazki-casovnik-nh34-640c703-657865817bf61_1920x1920.png?1702389168" alt="" />
+                    </div>
+                    <div className={styles["card-body"]}>
+                        <h4>Vostok-Europe Atomic Age Automatic Watch</h4>
+                        <div className={styles["price-box"]}>
+                            <span className={styles["old-price"]}>1905 лв.</span>
+                            <span className={styles["new-price"]}>1850 лв.</span>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles["watch-card"]}>
+                    <div className={styles["img-box"]}>
+                        <img src="https://cdncloudcart.com/16251/products/images/3711/vostok-black-edition-iv-benediktas-vanagas-46mm-mazki-casovnik-vk64-571j431-image_620507c29e21f_1280x1280.png?1644496866" alt="" />
+                    </div>
+                    <div className={styles["card-body"]}>
+                        <h4>Vostok-Europe SSN-571</h4>
+                        <div className={styles["price-box"]}>
+                            <span className={styles["reg-price"]}>1508 лв.</span>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles["watch-card"]}>
+                    <div className={styles["img-box"]}>
+                        <img src="https://cdncloudcart.com/16251/products/images/6537/vostok-europe-47mm-almaz-mazki-casovnik-6s11-320b660-65784878a30ae_600x600.png?1702381736" alt="" />
+                    </div>
+                    <div className={styles["card-body"]}>
+                        <h4>VOSTOK EUROPE 47ММ ALMAZ 6S11-320B660</h4>
+                        <div className={styles["price-box"]}>
+                            <span className={styles["reg-price"]}>950 лв.</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
