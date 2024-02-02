bcc7c0a7c093ab87b7c2a7f18170faf8093db282
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-02-02 16:08:06 +0200

    Add Login.jsx

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,27 @@
+import styles from "./Auth.module.css";
+
+function Login() {
+    return (
+        <div className={styles["container"]}>
+            <section className={styles["form-view"]}>
+                <h3>Вход</h3>
+                <form>
+                    <div className={styles["input"]}>
+                        <input type="text" name="email" placeholder="Имейл адрес" />
+                    </div>
+                    <div className={styles["input"]}>
+                        <input type="password" name="password" placeholder="Парола" />
+                    </div>
+                    <div className={styles["action"]}>
+                        <input type="submit" value="Влез"></input>
+                    </div>
+                    <div className={styles["no-reg"]}>
+                        <p>Нямаш регистрация? <a href="">Регистрирай се!</a> </p>
+                    </div>
+                </form>
+            </section>
+        </div>
+    );
+}
+
+export default Login;
