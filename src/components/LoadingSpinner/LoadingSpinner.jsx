146f0e221b73b9558d8de2ea115736cfb320a5fa
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-02-07 15:30:06 +0200

    Add LoadingSpinner.jsx

---
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+import styles from "./LoadingSpinner.module.css";
+
+function LoadingSpinner() {
+    return (
+        <span className={styles["loader"]}></span>
+    )
+}
+
+export default LoadingSpinner;
