71c26ae339d1a57853288d6dc72edbf6c948e7fd
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-02-09 12:19:48 +0200

    Add Watch.jsx

---
+++ src/components/WatchCatalog/Watch/Watch.jsx
@@ -0,0 +1,28 @@
+import styles from "./Watch.module.css";
+
+function Watch({ watch }) {
+    return (
+        <div className={styles["watch-card"]}>
+            <div className={styles["img-box"]}>
+                <img src={watch.imageUrl} alt="" />
+            </div>
+            <div className={styles["card-body"]}>
+                <h4>{watch.title}</h4>
+
+                {
+                    watch.salePrice && watch.salePrice < watch.price
+                        ? <div className={styles["price-box"]}>
+                            <span className={styles["old-price"]}>{watch.price} лв.</span>
+                            <span className={styles["new-price"]}>{watch.salePrice} лв.</span>
+                        </div>
+                        : <div className={styles["price-box"]}>
+                            <span className={styles["reg-price"]}>{watch.price} лв.</span>
+                        </div>
+                }
+
+            </div>
+        </div>
+    );
+}
+
+export default Watch;
