b2a5b7440c5fc8535e7afa869ac4a6ab905b350e
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-02-09 12:21:23 +0200

    Add functions getAllPaginated and getWatchesCount

--- src/services/watchService.js
+++ src/services/watchService.js
@@ -3,7 +3,9 @@ import * as api from "./api";
 const endpoints = {
     addWatch: '/watches',
     getAll: '/watches',
-    getBrandsLogo: '/watches/brandsLogo'
+    getBrandsLogo: '/watches/brandsLogo',
+    getAllPaginated: (page, limit) => `/watches/paginated?page=${page}&limit=${limit}`,
+    getWatchesCount: '/watches/count'
 };
 
 export const addWatch = (data) => api.post(endpoints.addWatch, data);
@@ -11,3 +13,7 @@ export const addWatch = (data) => api.post(endpoints.addWatch, data);
 export const getAll = () => api.get(endpoints.getAll);
 
 export const getBrandsLogo = () => api.get(endpoints.getBrandsLogo);
+
+export const getAllPaginated = (page, limit) => api.get(endpoints.getAllPaginated(page, limit));
+
+export const getWatchesCount = () => api.get(endpoints.getWatchesCount);
