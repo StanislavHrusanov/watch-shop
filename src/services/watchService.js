4702ab148612227e96629f1617f577bf824d4f54
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-02-12 16:35:47 +0200

    Delete getWatchesByTypePaginated and getWatchesByTypeCount

--- src/services/watchService.js
+++ src/services/watchService.js
@@ -6,8 +6,6 @@ const endpoints = {
     getBrandsLogo: '/watches/brandsLogo',
     getAllPaginated: (filterCriteria, sortCriteria, page, limit) => `/watches/paginated?filteredByCriteria=${filterCriteria}&sortedByCriteria=${sortCriteria}&page=${page}&limit=${limit}`,
     getWatchesCount: (type, brand) => `/watches/count?type=${type}&brand=${brand}`,
-    getWatchesByTypePaginated: (type, page, limit) => `/watches/types/${type}?page=${page}&limit=${limit}`,
-    getWatchesByTypeCount: (type) => `/watches/count/types/${type}`,
     getWatchesByBrandPaginated: (brand, page, limit) => `/watches/brands/${brand}?page=${page}&limit=${limit}`,
     getWatchesByBrandCount: (brand) => `/watches/count/brands/${brand}`
 };
@@ -21,10 +19,6 @@ export const getBrandsLogo = () => api.get(endpoints.getBrandsLogo);
 export const getAllPaginated = (filterCriteria, sortCriteria, page, limit) => api.get(endpoints.getAllPaginated(filterCriteria, sortCriteria, page, limit));
 
 export const getWatchesCount = (type, brand) => api.get(endpoints.getWatchesCount(type, brand));
-
-export const getWatchesByTypePaginated = (type, page, limit) => api.get(endpoints.getWatchesByTypePaginated(type, page, limit));
-
-export const getWatchesByTypeCount = (type) => api.get(endpoints.getWatchesByTypeCount(type));
 
 export const getWatchesByBrandPaginated = (brand, page, limit) => api.get(endpoints.getWatchesByBrandPaginated(brand, page, limit));
 
