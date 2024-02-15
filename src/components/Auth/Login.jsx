383dea306c2ce00a87f0dfb1312f05692e5b0e82
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-02-15 14:56:22 +0200

    Add functionality to login

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,27 +1,86 @@
 import styles from "./Form.module.css";
+import { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
+import { LoadingContext } from "../../contexts/LoadingContext";
+import { AuthContext } from "../../contexts/AuthContext";
+import * as authService from "../../services/authService";
+import { trimInputs } from "../../utils";
 
 function Login() {
-    return (
-        <div className={styles["container"]}>
-            <section className={styles["form-view"]}>
-                <h3>Вход</h3>
-                <form>
-                    <div className={styles["input"]}>
-                        <input type="text" name="email" placeholder="Имейл адрес" />
-                    </div>
-                    <div className={styles["input"]}>
-                        <input type="password" name="password" placeholder="Парола" />
-                    </div>
-                    <div className={styles["action"]}>
-                        <input type="submit" value="Влез"></input>
-                    </div>
-                    <div className={styles["no-reg"]}>
-                        <p>Нямаш регистрация? <a href="">Регистрирай се!</a> </p>
-                    </div>
-                </form>
-            </section>
-        </div>
-    );
+    const [inputs, setInputs] = useState({
+        email: '',
+        password: ''
+    });
+
+    const { userLogin } = useContext(AuthContext);
+    const { isLoading, showLoading, hideLoading } = useContext(LoadingContext);
+    const navigate = useNavigate();
+
+    const onChangeHandler = (e) => {
+        setInputs(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }));
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            showLoading();
+            trimInputs(inputs);
+            const loggedInUser = await authService.login(inputs);
+            userLogin(loggedInUser);
+            hideLoading();
+            navigate('/');
+
+        } catch (error) {
+            window.alert(error.message);
+            hideLoading();
+            return navigate('/login');
+        }
+    }
+
+    return isLoading
+        ? (
+            <LoadingSpinner />
+        )
+        : (
+            <div className={styles["container"]}>
+                <section className={styles["form-view"]}>
+                    <h3>Вход</h3>
+                    <form onSubmit={onSubmit}>
+                        <div className={styles["input"]}>
+                            <input
+                                type="text"
+                                name="email"
+                                id="email"
+                                placeholder="Имейл адрес"
+                                value={inputs.email}
+                                onChange={onChangeHandler}
+                            />
+                        </div>
+                        <div className={styles["input"]}>
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                placeholder="Парола"
+                                value={inputs.password}
+                                onChange={onChangeHandler}
+                            />
+                        </div>
+                        <div className={styles["action"]}>
+                            <input type="submit" value="Влез"></input>
+                        </div>
+                        <div className={styles["no-reg"]}>
+                            <p>Нямаш регистрация? <Link to="/register">Регистрирай се!</Link> </p>
+                        </div>
+                    </form>
+                </section>
+            </div>
+        );
 }
 
 export default Login;
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,8 +1,11 @@
 import * as api from "./api";
 
 const endpoints = {
-    register: '/auth/register'
+    register: '/auth/register',
+    login: '/auth/login'
 };
 
 export const register = (userData) => api.post(endpoints.register, userData);
+
+export const login = (userData) => api.post(endpoints.login, userData);
 
