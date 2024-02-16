2c5c82404271a7d20efb806bc6ff902498cb4852
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-02-16 12:44:54 +0200

    Add my profile dropdown

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
     const [isNavToggled, setIsNavToggled] = useState(false);
+    const [isDropdownToggled, setIsDropdownToggled] = useState(false);
 
     const { user, userLogout } = useContext(AuthContext);
 
@@ -29,7 +30,29 @@ function Navbar() {
                 <Link className={styles["nav-link"]} to="/watches/types/women">Дамски</Link>
                 {
                     user
-                        ? <Link onClick={() => userLogout()} className={styles["nav-link"]}>Изход</Link>
+                        ? <>
+                            <div className={styles["nav-link-dropdown"]}>
+                                <div className={styles["my-profile"]} onClick={() => setIsDropdownToggled(!isDropdownToggled)}>Моят профил<i
+                                    className="fa fa-chevron-down"></i></div>
+                                {
+                                    isDropdownToggled &&
+                                    <div onMouseLeave={() => setIsDropdownToggled(false)} className={styles["dropdown-content"]}>
+                                        {
+                                            user?.isAdmin
+                                                ? <>
+                                                    <Link to="/addWatch">Добави часовник</Link>
+                                                    <Link to="/purchases">Поръчки</Link>
+                                                </>
+                                                : <>
+                                                    <Link to="/wishlist">Любими</Link>
+                                                    <Link to="/myOrders">Поръчки</Link>
+                                                </>
+                                        }
+                                    </div>
+                                }
+                            </div>
+                            <Link onClick={() => userLogout()} className={styles["nav-link"]}>Изход</Link>
+                        </>
                         : <>
                             <Link className={styles["nav-link"]} to="/register">Регистрация</Link>
                             <Link className={styles["nav-link"]} to="/login">Вход</Link>
@@ -45,7 +68,29 @@ function Navbar() {
                     <Link className={styles["nav-link"]} to="/watches/types/women">Дамски</Link>
                     {
                         user
-                            ? <Link onClick={() => userLogout()} className={styles["nav-link"]}>Изход</Link>
+                            ? <>
+                                <div className={styles["nav-link-dropdown"]}>
+                                    <div className={styles["my-profile"]} onClick={() => setIsDropdownToggled(!isDropdownToggled)}>Моят профил<i
+                                        className="fa fa-chevron-down"></i></div>
+                                    {
+                                        isDropdownToggled &&
+                                        <div onTouchMove={() => setIsDropdownToggled(false)} className={styles["dropdown-content"]}>
+                                            {
+                                                user?.isAdmin
+                                                    ? <>
+                                                        <Link to="/addWatch">Добави часовник</Link>
+                                                        <Link to="/purchases">Поръчки</Link>
+                                                    </>
+                                                    : <>
+                                                        <Link to="/wishlist">Любими</Link>
+                                                        <Link to="/myOrders">Поръчки</Link>
+                                                    </>
+                                            }
+                                        </div>
+                                    }
+                                </div>
+                                <Link onClick={() => userLogout()} className={styles["nav-link"]}>Изход</Link>
+                            </>
                             : <>
                                 <Link className={styles["nav-link"]} to="/register">Регистрация</Link>
                                 <Link className={styles["nav-link"]} to="/login">Вход</Link>
