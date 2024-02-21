b93fdc764f4a44df7b6692ab34af72711d583343
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-02-21 14:04:29 +0200

    Add useEffect to get and set fresh user info

--- src/components/MyProfile/Wishlist/Wishlist.jsx
+++ src/components/MyProfile/Wishlist/Wishlist.jsx
@@ -1,15 +1,34 @@
 import styles from "./Wishlist.module.css";
-import { useContext } from "react";
+import { useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import LoadingSpinner from "../../LoadingSpinner/LoadingSpinner";
 import { AuthContext } from "../../../contexts/AuthContext";
+import { LoadingContext } from "../../../contexts/LoadingContext";
+import { UserProfileContext } from "../../../contexts/UserContext";
 import * as myProfileService from "../../../services/myProfileService";
 import WishedWatchCard from "./WishedWatchCard/WishedWatchCard";
-import { UserProfileContext } from "../../../contexts/UserContext";
 
 function Wishlist() {
     const { user } = useContext(AuthContext);
     const { userInfo, setUserInfo } = useContext(UserProfileContext);
+    const { isLoading, showLoading, hideLoading } = useContext(LoadingContext);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        (async () => {
+            try {
+                showLoading();
+                const freshUserInfo = await myProfileService.getUserInfo(user._id);
+                setUserInfo(freshUserInfo);
+                hideLoading();
+
+            } catch (error) {
+                window.alert(error.message);
+                hideLoading();
+                return navigate('/wishlist');
+            }
+        })();
+    }, [showLoading, hideLoading, navigate, setUserInfo, user._id]);
 
     const onRemoveFromWishlist = async (watchForRemoving) => {
 
@@ -27,22 +46,26 @@ function Wishlist() {
         }
     }
 
-    return (
-        <div className={styles["container"]}>
-            <h3>Любими</h3>
-            <div className={styles["row-cards"]}>
-                {
-                    userInfo.wishlist.length > 0
-                        ? userInfo.wishlist.map(x => <WishedWatchCard key={x._id} watch={x} onRemoveFromWishlist={onRemoveFromWishlist} />)
-                        : <div className={styles["no-watches-container"]}>
-                            <p className={styles["no-watches-message"]}>
-                                Все още няма  добавени часовници в любими!
-                            </p>
-                        </div>
-                }
+    return isLoading
+        ? (
+            <LoadingSpinner />
+        )
+        : (
+            <div className={styles["container"]}>
+                <h3>Любими</h3>
+                <div className={styles["row-cards"]}>
+                    {
+                        userInfo.wishlist.length > 0
+                            ? userInfo.wishlist.map(x => <WishedWatchCard key={x._id} watch={x} onRemoveFromWishlist={onRemoveFromWishlist} />)
+                            : <div className={styles["no-watches-container"]}>
+                                <p className={styles["no-watches-message"]}>
+                                    Все още няма  добавени часовници в любими!
+                                </p>
+                            </div>
+                    }
+                </div>
             </div>
-        </div>
-    );
+        );
 }
 
 export default Wishlist;
