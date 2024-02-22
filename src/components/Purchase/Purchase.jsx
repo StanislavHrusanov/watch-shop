4327a137aaf04db35a46d17c6e7f9a65be98e072
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-02-22 15:21:46 +0200

    Add div with purchase info if purchase is finished

--- src/components/Purchase/Purchase.jsx
+++ src/components/Purchase/Purchase.jsx
@@ -1,5 +1,5 @@
 import styles from "./Purchase.module.css";
-import { useEffect, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 import { AuthContext } from "../../contexts/AuthContext";
@@ -9,6 +9,7 @@ import * as myProfileService from "../../services/myProfileService";
 import * as utils from "../../utils";
 
 function Purchase() {
+    const [isPurchaseMade, setIsPurchaseMade] = useState(false);
     const { user } = useContext(AuthContext);
     const { userInfo, setUserInfo } = useContext(UserProfileContext);
     const { isLoading, showLoading, hideLoading } = useContext(LoadingContext);
@@ -32,58 +33,65 @@ function Purchase() {
     return isLoading
         ? (
             <LoadingSpinner />
-        )
-        : (
-            <div className={styles["container"]}>
-                <h3>Потвърди и плати</h3>
-                <div className={styles["col"]}>
-                    <div className={styles["delivery-info"]}>
-                        <h4>Данни за доставка</h4>
-                        <p className={styles["names"]}>{userInfo.firstName} {userInfo.lastName}</p>
-                        <p className={styles["add-info"]}>{userInfo.address}</p>
-                        <p className={styles["add-info"]}>{userInfo.phoneNumber}</p>
-                    </div>
-                    {/* <div className={styles["delivery-info-no-user"]}>
-                    <h4>Поръчка без регистрация</h4>
-                    <div className={styles["input"]}>
-                        <input type="text" name="firstName" id="firstName" placeholder="Име" />
-                    </div>
-                    <div className={styles["input"]}>
-                        <input type="text" name="lastName" id="lastName" placeholder="Фамилия" />
-                    </div>
-                    <div className={styles["input"]}>
-                        <input type="text" name="addresss" id="address" placeholder="Адрес" />
-                    </div>
-                    <div className={styles["input"]}>
-                        <input type="text" name="phoneNumber" id="phoneNumber" placeholder="Телефон" />
-                    </div>
-                </div> */}
-                    <div className={styles["pay-method"]}>
-                        <h4>Метод на плащане</h4>
-                        <p className={styles["method-type"]}>Наложен платеж</p>
-                        <p className={styles["method-info"]}>Плащането се извършва в брой или с карта при получаване на продукта.</p>
-                    </div>
-                    <div className={styles["confirm-and-pay"]}>
-                        <h4>Преглед на поръчката</h4>
-                        <div className={styles["items-value-box"]}>
-                            <p>Стойност на продуктите:</p>
-                            <span>{utils.getTotalPrice(userInfo.cart)} лв.</span>
+        ) : isPurchaseMade
+            ? (
+                <div className={styles["purchase-info-container"]}>
+                    <p>Успешно направихте поръчка с № 2131231212321</p>
+                    <p>Благодарим Ви!</p>
+                    <div className={styles["to-my-purchases"]}>Към моите поръчки</div>
+                </div>
+            )
+            : (
+                <div className={styles["container"]}>
+                    <h3>Потвърди и плати</h3>
+                    <div className={styles["col"]}>
+                        <div className={styles["delivery-info"]}>
+                            <h4>Данни за доставка</h4>
+                            <p className={styles["names"]}>{userInfo.firstName} {userInfo.lastName}</p>
+                            <p className={styles["add-info"]}>{userInfo.address}</p>
+                            <p className={styles["add-info"]}>{userInfo.phoneNumber}</p>
                         </div>
-                        <div className={styles["delivery-box"]}>
-                            <p>Доставка:</p>
-                            <span>Безплатно</span>
+                        {/* <div className={styles["delivery-info-no-user"]}>
+                <h4>Поръчка без регистрация</h4>
+                <div className={styles["input"]}>
+                    <input type="text" name="firstName" id="firstName" placeholder="Име" />
+                </div>
+                <div className={styles["input"]}>
+                    <input type="text" name="lastName" id="lastName" placeholder="Фамилия" />
+                </div>
+                <div className={styles["input"]}>
+                    <input type="text" name="addresss" id="address" placeholder="Адрес" />
+                </div>
+                <div className={styles["input"]}>
+                    <input type="text" name="phoneNumber" id="phoneNumber" placeholder="Телефон" />
+                </div>
+            </div> */}
+                        <div className={styles["pay-method"]}>
+                            <h4>Метод на плащане</h4>
+                            <p className={styles["method-type"]}>Наложен платеж</p>
+                            <p className={styles["method-info"]}>Плащането се извършва в брой или с карта при получаване на продукта.</p>
                         </div>
-                        <div className={styles["total-box"]}>
-                            <p>Общо:</p>
-                            <span>{utils.getTotalPrice(userInfo.cart)} лв.</span>
-                        </div>
-                        <div className={styles["confirm-btn"]}>
-                            <p>Потвърди и плати</p>
+                        <div className={styles["confirm-and-pay"]}>
+                            <h4>Преглед на поръчката</h4>
+                            <div className={styles["items-value-box"]}>
+                                <p>Стойност на продуктите:</p>
+                                <span>{utils.getTotalPrice(userInfo.cart)} лв.</span>
+                            </div>
+                            <div className={styles["delivery-box"]}>
+                                <p>Доставка:</p>
+                                <span>Безплатно</span>
+                            </div>
+                            <div className={styles["total-box"]}>
+                                <p>Общо:</p>
+                                <span>{utils.getTotalPrice(userInfo.cart)} лв.</span>
+                            </div>
+                            <div className={styles["confirm-btn"]}>
+                                <p>Потвърди и плати</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
 }
 
 export default Purchase;
