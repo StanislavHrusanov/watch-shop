75117a7294b5fb24ee7121a7edfc985bb100134b
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-02-24 15:14:13 +0200

    Get user info by useEffect in UserProfileContext

--- src/contexts/UserProfileContext.jsx
+++ src/contexts/UserProfileContext.jsx
@@ -1,12 +1,28 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "./AuthContext";
+import * as myProfileService from "../services/myProfileService";
 
 export const UserProfileContext = createContext();
 
 export const UserProfileProvider = ({ children }) => {
-    const [userInfo, setUserInfo] = useState({
-        wishlist: [],
-        cart: []
-    });
+    const [userInfo, setUserInfo] = useState({});
+    const { user } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        (async () => {
+            try {
+                if (user) {
+                    const userDetails = await myProfileService.getUserInfo(user._id);
+                    setUserInfo(userDetails);
+                }
+            } catch (error) {
+                window.alert(error.message);
+                return navigate('/');
+            }
+        })();
+    }, [user, navigate]);
 
     return (
         <UserProfileContext.Provider value={{
