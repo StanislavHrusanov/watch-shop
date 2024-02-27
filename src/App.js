6a0163755e346208ce9c039529970d7ad7b2feed
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-02-27 14:23:44 +0200

    Add routGuards to app

--- src/App.js
+++ src/App.js
@@ -19,11 +19,16 @@ import Purchase from './components/Purchase/Purchase';
 import Orders from './components/AdminProfile/Orders/Orders';
 import WatchCatalogType from './components/WatchCatalog/WatchCatalogType/WatchCatalogType';
 import WatchCatalogBrand from './components/WatchCatalog/WatchCatalogBrand/WatchCatalogBrand';
+import NotFound from './components/NotFound/NotFound';
 
 import { LoadingProvider } from './contexts/LoadingContext';
 import { PageProvider } from './contexts/PageContext';
 import { AuthProvider } from './contexts/AuthContext';
 import { UserProfileProvider } from './contexts/UserProfileContext';
+import { AlreadyLoggedIn } from './components/RoutGuards/AlreadyLoggedIn';
+import { IsGuest } from './components/RoutGuards/IsGuest';
+import { IsAdmin } from './components/RoutGuards/IsAdmin';
+import { IsNormalUser } from './components/RoutGuards/IsNormalUser';
 
 function App() {
   return (
@@ -43,16 +48,38 @@ function App() {
                   <Route path='/watches/types/:type' element={<WatchCatalogType />} />
                   <Route path='/watches/brands/:brand' element={<WatchCatalogBrand />} />
                   <Route path='/watches/:watchId' element={<Details />} />
-                  <Route path='/watches/:watchId/edit' element={<Edit />} />
-                  <Route path='/register' element={<Register />} />
-                  <Route path='/login' element={<Login />} />
-                  <Route path='/logout' element={<Logout />} />
-                  <Route path='/addWatch' element={<AddWatch />} />
-                  <Route path='/wishlist' element={<Wishlist />} />
-                  <Route path='/myOrders' element={<MyOrders />} />
-                  <Route path='/cart' element={<Cart />} />
-                  <Route path='/purchase' element={<Purchase />} />
-                  <Route path='/orders' element={<Orders />} />
+
+                  <Route element={<AlreadyLoggedIn />}>
+
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/login' element={<Login />} />
+
+                  </Route>
+
+                  <Route element={<IsGuest />}>
+
+                    <Route path='/logout' element={<Logout />} />
+
+                    <Route element={<IsAdmin />}>
+
+                      <Route path='/wishlist' element={<Wishlist />} />
+                      <Route path='/cart' element={<Cart />} />
+                      <Route path='/purchase' element={<Purchase />} />
+                      <Route path='/myOrders' element={<MyOrders />} />
+
+                    </Route>
+
+                    <Route element={<IsNormalUser />}>
+
+                      <Route path='/addWatch' element={<AddWatch />} />
+                      <Route path='/watches/:watchId/edit' element={<Edit />} />
+                      <Route path='/orders' element={<Orders />} />
+
+                    </Route>
+
+                  </Route>
+
+                  <Route path='/*' element={<NotFound />} />
 
                 </Routes>
               </div>
