58d1e8054c8ead94a3c6758ed76f610a24993559
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-02-27 14:21:49 +0200

    Add div with message no watches added yet if user cart is empty

--- src/components/Purchase/Purchase.jsx
+++ src/components/Purchase/Purchase.jsx
@@ -67,52 +67,45 @@ function Purchase() {
             : (
                 <div className={styles["container"]}>
                     <h3>Потвърди и плати</h3>
-                    <div className={styles["col"]}>
-                        <div className={styles["delivery-info"]}>
-                            <h4>Данни за доставка</h4>
-                            <p className={styles["names"]}>{userInfo.firstName} {userInfo.lastName}</p>
-                            <p className={styles["add-info"]}>{userInfo.address}</p>
-                            <p className={styles["add-info"]}>{userInfo.phoneNumber}</p>
-                        </div>
-                        {/* <div className={styles["delivery-info-no-user"]}>
-                <h4>Поръчка без регистрация</h4>
-                <div className={styles["input"]}>
-                    <input type="text" name="firstName" id="firstName" placeholder="Име" />
-                </div>
-                <div className={styles["input"]}>
-                    <input type="text" name="lastName" id="lastName" placeholder="Фамилия" />
-                </div>
-                <div className={styles["input"]}>
-                    <input type="text" name="addresss" id="address" placeholder="Адрес" />
-                </div>
-                <div className={styles["input"]}>
-                    <input type="text" name="phoneNumber" id="phoneNumber" placeholder="Телефон" />
-                </div>
-            </div> */}
-                        <div className={styles["pay-method"]}>
-                            <h4>Метод на плащане</h4>
-                            <p className={styles["method-type"]}>Наложен платеж</p>
-                            <p className={styles["method-info"]}>Плащането се извършва в брой или с карта при получаване на продукта.</p>
-                        </div>
-                        <div className={styles["confirm-and-pay"]}>
-                            <h4>Преглед на поръчката</h4>
-                            <div className={styles["items-value-box"]}>
-                                <p>Стойност на продуктите:</p>
-                                <span>{utils.getTotalPrice(userInfo.cart)} лв.</span>
+                    {
+                        userInfo.cart?.length === 0
+                            ? <div className={styles["no-watches-container"]}>
+                                <p className={styles["no-watches-message"]}>
+                                    Все още няма  добавени часовници в количката!
+                                </p>
                             </div>
-                            <div className={styles["delivery-box"]}>
-                                <p>Доставка:</p>
-                                <span>Безплатно</span>
+                            : <div className={styles["col"]}>
+                                <div className={styles["delivery-info"]}>
+                                    <h4>Данни за доставка</h4>
+                                    <p className={styles["names"]}>{userInfo.firstName} {userInfo.lastName}</p>
+                                    <p className={styles["add-info"]}>{userInfo.address}</p>
+                                    <p className={styles["add-info"]}>{userInfo.phoneNumber}</p>
+                                </div>
+                                <div className={styles["pay-method"]}>
+                                    <h4>Метод на плащане</h4>
+                                    <p className={styles["method-type"]}>Наложен платеж</p>
+                                    <p className={styles["method-info"]}>Плащането се извършва в брой или с карта при получаване на продукта.</p>
+                                </div>
+                                <div className={styles["confirm-and-pay"]}>
+                                    <h4>Преглед на поръчката</h4>
+                                    <div className={styles["items-value-box"]}>
+                                        <p>Стойност на продуктите:</p>
+                                        <span>{utils.getTotalPrice(userInfo.cart)} лв.</span>
+                                    </div>
+                                    <div className={styles["delivery-box"]}>
+                                        <p>Доставка:</p>
+                                        <span>Безплатно</span>
+                                    </div>
+                                    <div className={styles["total-box"]}>
+                                        <p>Общо:</p>
+                                        <span>{utils.getTotalPrice(userInfo.cart)} лв.</span>
+                                    </div>
+                                    <div className={styles["confirm-btn"]}>
+                                        <p onClick={onCreateOrder}>Потвърди и плати</p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className={styles["total-box"]}>
-                                <p>Общо:</p>
-                                <span>{utils.getTotalPrice(userInfo.cart)} лв.</span>
-                            </div>
-                            <div className={styles["confirm-btn"]}>
-                                <p onClick={onCreateOrder}>Потвърди и плати</p>
-                            </div>
-                        </div>
-                    </div>
+                    }
                 </div>
             );
 }
