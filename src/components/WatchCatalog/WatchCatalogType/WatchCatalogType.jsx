53e9c4d47fc7f551badc805c45ce49568c389b60
StanislavHrusanov <stanislavhrusanov@gmail.com>
2024-03-06 16:17:36 +0200

    Update WatchCatalogType to show queries in url

--- src/components/WatchCatalog/WatchCatalogType/WatchCatalogType.jsx
+++ src/components/WatchCatalog/WatchCatalogType/WatchCatalogType.jsx
@@ -5,11 +5,9 @@ import Watch from "../Watch/Watch";
 import LoadingSpinner from "../../LoadingSpinner/LoadingSpinner";
 import * as watchService from "../../../services/watchService";
 import { LoadingContext } from "../../../contexts/LoadingContext";
-import { PageContext } from "../../../contexts/PageContext";
 
 function WatchCatalogType() {
     const [watches, setWatches] = useState([]);
-    const [watchCount, setWatchCount] = useState();
     const [page, setPage] = useState(1);
     const [limit] = useState(4);
     const [pages, setPages] = useState(1);
@@ -17,27 +15,22 @@ function WatchCatalogType() {
     const { type } = useParams();
 
     const { isLoading, showLoading, hideLoading } = useContext(LoadingContext);
-    const { watchType, setWatchType } = useContext(PageContext);
     const navigate = useNavigate();
+
+    const url = new URL(window.location.href);
+    const pageQuery = url.searchParams.get('page');
+    const sortQuery = url.searchParams.get('sortedBy');
 
     useEffect(() => {
         (async () => {
             try {
                 showLoading();
                 const allWatches = await watchService.getAllPaginated(type, sortCriteria, page, limit);
-                const allWatchesCount = await watchService.getWatchesCount(type, 'all');
+                const allWatchesCount = await watchService.getWatchesCount(type, 'all', '');
                 setWatches(allWatches);
-                setWatchCount(allWatchesCount);
-                setPages(Math.ceil(watchCount / limit));
-                // setPage(state => type === watchType ? state : 1);
-                setPage((state) => {
-                    if (type !== watchType) {
-                        setSortCriteria('newest');
-                        return 1;
-                    }
-                    return state;
-                });
-                setWatchType(type);
+                setPages(Math.ceil(allWatchesCount / limit) || 1);
+                setPage(pageQuery ? Number(pageQuery) : 1);
+                setSortCriteria(sortQuery || 'newest');
                 hideLoading();
 
             } catch (error) {
@@ -46,21 +39,39 @@ function WatchCatalogType() {
                 navigate('/watches');
             }
         })();
-    }, [showLoading, hideLoading, navigate, type, page, limit, watchCount, watchType, setWatchType, sortCriteria]);
+    }, [
+        showLoading,
+        hideLoading,
+        navigate,
+        type,
+        page,
+        limit,
+        sortCriteria,
+        pageQuery,
+        sortQuery
+    ]);
 
     const onSort = (criteria) => {
-        setSortCriteria(criteria)
+        navigate(`?sortedBy=${criteria}`);
     }
 
     const prevPage = () => {
         if (page > 1) {
-            setPage(state => state - 1);
+            if (sortQuery) {
+                navigate(`?sortedBy=${sortQuery}&page=${page - 1}`);
+            } else {
+                navigate(`?page=${page - 1}`);
+            }
         }
     }
 
     const nextPage = () => {
         if (page < pages) {
-            setPage(state => state + 1);
+            if (sortQuery) {
+                navigate(`?sortedBy=${sortQuery}&page=${page + 1}`);
+            } else {
+                navigate(`?page=${page + 1}`);
+            }
         }
     }
 
@@ -72,21 +83,13 @@ function WatchCatalogType() {
             <div className={styles["container"]}>
                 <h3>{type === 'men' ? 'Мъжки' : 'Дамски'} часовници</h3>
                 <div className={styles["row-sort-filter"]}>
-                    <div className={styles["filter"]}>
-                        <p>Тип:</p>
-                        <select name="" id="">
-                            <option value="Всички">Всички</option>
-                            <option value="Мъжки">Мъжки</option>
-                            <option value="Дамски">Дамски</option>
-                        </select>
-                    </div>
+
                     <div className={styles["sort"]}>
                         <p>Сортирай по:</p>
                         <select
                             onChange={(e) => onSort(e.target.value)}
                             value={sortCriteria}
                         >
-                            <option value=""></option>
                             <option value="newest">Най-нови</option>
                             <option value="lowestPrice">Най-ниска цена</option>
                             <option value="highestPrice">Най-висока цена</option>
@@ -96,7 +99,14 @@ function WatchCatalogType() {
 
                 <div className={styles["row-cards"]}>
 
-                    {watches.map(x => <Watch key={x._id} watch={x} />)}
+                    {watches.length > 0
+                        ? watches.map(x => <Watch key={x._id} watch={x} />)
+                        : <div className={styles["no-watches-container"]}>
+                            <p className={styles["no-watches-message"]}>
+                                Все още няма добавени часовници!
+                            </p>
+                        </div>
+                    }
 
                 </div>
                 <div className={styles["pagination"]}>
